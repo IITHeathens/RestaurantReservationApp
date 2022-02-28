8038298ec7c8c412e6cb685e3585ce3480ce276b
Jiehfeng <alain.20200122@iit.ac.lk>
2022-02-28 23:07:57 +0530

    Edited Main Activity name.

--- app/src/main/java/iit/heathens/reservationapp/MainActivity.kt
+++ app/src/main/java/iit/heathens/reservationapp/MainActivity.kt
@@ -3,7 +3,7 @@ package iit.heathens.reservationapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
