6074bf207a88abd066b1f9cf72e49bde02f6b3a8
Jiehfeng <alain.20200122@iit.ac.lk>
2022-03-01 17:39:45 +0530

    Added various activities for our app

---
+++ app/src/main/java/iit/heathens/reservationapp/FactorPage.kt
@@ -0,0 +1,11 @@
+package iit.heathens.reservationapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FactorPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_factor_page)
+    }
+}
