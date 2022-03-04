010f6235f69d4ab95f08155019d8fb4029679ece
Jiehfeng <alain.20200122@iit.ac.lk>
2022-03-04 14:38:26 +0530

    Added Firebase Integration & Tested

--- app/src/main/java/iit/heathens/reservationapp/PrototypeTester.java
+++ app/src/main/java/iit/heathens/reservationapp/PrototypeTester.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class activity_prototype_tester extends AppCompatActivity {
+public class PrototypeTester extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
