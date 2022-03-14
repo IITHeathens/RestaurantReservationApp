9a567be99c27dab152c6dac33c694997445fc6f0
Jiehfeng <alain.20200122@iit.ac.lk>
2022-03-14 15:21:11 +0530

    Major improvements to the AI Predictions Server

--- No Shows Prediction AI/AI_Server.py
+++ No Shows Prediction AI/AI_Server.py
@@ -3,86 +3,92 @@ import re
 import time
 import random
 import sys
-from firebase import firebase
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
 
 # Set our Google Firebase Database
-firebase = firebase.FirebaseApplication("https://resresapp-default-rtdb.asia-southeast1.firebasedatabase.app/", None)
+cred = credentials.Certificate("resresapp-firebase-adminsdk-f9ios-dfb77556fd.json")
+firebase_admin.initialize_app(cred, {
+    "databaseURL" : "https://resresapp-default-rtdb.asia-southeast1.firebasedatabase.app/"
+})
 
-first_name = ""
-first_factor_name = ""
+firebase_app_status = db.reference("/Python AI Server/Server Status")
 
-if firebase.get("/Python AI Server/Server Status", "") is None:
-    data = {
-        "Status" : "Operational"
-    }
+# firebase_app = firebase.FirebaseApplication("https://resresapp-default-rtdb.asia-southeast1.firebasedatabase.app/", None)
 
-    output = firebase.post("/Python AI Server/Server Status", data)
+data = {
+    "Status" : "Operational"
+}
 
-if firebase.get("/Python AI Server/Predictions", "") is None:
+firebase_app_status.set(data)
+
+
+predictions_count = 0
+predictions_path = "/Python AI Server/Predictions/"
+firebase_app_predictions = db.reference("/Python AI Server/Predictions/0")
+
+
+def create_prediction_results():
     # POST empty Prediction to Real-Time Database
+    global data
     data = {
         "Factors" : "",
         "Prediction" : "None",
         "Probability" : "None"
     }
 
-    output = firebase.post("/Python AI Server/Predictions", data)
-    print(output)
-    first_name = output['name']
+    firebase_app_predictions.set(data)
 
-    print()
 
-    result = firebase.get("/Python AI Server/Predictions", "")
-    print(result)
-else:
-    pass
+create_prediction_results()
 
+inputs_count = 0
+inputs_path = "/Python AI Server/Inputs/"
+firebase_app_inputs = db.reference("/Python AI Server/Inputs/0")
+check = firebase_app_inputs.get()
+print(type(check))
 
 def create_input_factors():
-    global first_factor_name
+    #global first_factor_name
 
-    if firebase.get("/Python AI Server/Input Factors", "") is None:
-        # POST empty Prediction to Real-Time Database
-        factor_data = {
-            "Parking" : "",
-            "Percentage Distance" : "",
-            "Refundable Deposit" : ""
-        }
+    # POST empty Prediction to Real-Time Database
+    factor_data = {
+        "Parking" : "",
+        "Percentage Distance" : "",
+        "Refundable Deposit" : ""
+    }
 
-        data = {
-            "Factors" : factor_data,
-            "Done" : "No"
-        }
+    data_data = {
+        "Factors" : factor_data,
+        "Done" : "No"
+    }
 
-        output = firebase.post("/Python AI Server/Input Factors", data)
-        print(output)
-        first_factor_name = output['name']
-
-        print()
-
-        result = firebase.get("/Python AI Server/Input Factors", "")
-        print(result)
-    else:
-        pass
-
+    firebase_app_inputs.set(data_data)
+    print()
 
 print("Again")
 create_input_factors()
 
-entry_names = [first_name]
-factor_names = [first_factor_name]
-
 
 def mainloop():
     # Server Start
-    status = firebase.get("/Python AI Server/Server Status", "")
-    operation_name = list(status)[0]
-    status_dict = list(status.values())[0]
-    operation_output = firebase.put("/Python AI Server/Server Status/" + operation_name, 'Status', 'Operational')
-    print("Operation output is ", str(operation_output))
+    # status = firebase_app_status.get()
+    # operation_name = list(status)[0]
+    # status_dict = list(status.values())[0]
+    # operation_output = firebase_app.put("/Python AI Server/Server Status/" + operation_name, 'Status', 'Operational')
+    # print("Operation output is ", str(operation_output))
 
-    count = 1
-    global first_name, entry_names, factor_names
+    global predictions_count, predictions_path, inputs_count, inputs_path
+    global firebase_app_predictions, firebase_app_inputs
+
+    firebase_app_predictions = db.reference(predictions_path + str(predictions_count))
+    firebase_app_inputs = db.reference(inputs_path + str(inputs_count))
+
+    # inputs_count = 0
+    # inputs_path = "/Python AI Server/Inputs/"
+    # firebase_app_inputs = db
 
     parking = 0
     distance = 100.0
@@ -91,71 +97,63 @@ def mainloop():
     # Heroku Mainloop
     while True:
         # Server Status
-        status = firebase.get("/Python AI Server/Server Status", "")
-        status_dict = list(status.values())[0]
-        if status_dict['Status'] == "Closed":
-            break
+        # status = firebase_app.get("/Python AI Server/Server Status", "")
+        # status_dict = list(status.values())[0]
+        # if status_dict['Status'] == "Closed":
+        #     break
 
-        factors = firebase.get("/Python AI Server/Input Factors", "")
-        name = ""
+        firebase_app_predictions = db.reference(predictions_path + str(predictions_count))
+        firebase_app_inputs = db.reference(inputs_path + str(inputs_count))
 
-        counter = 1
+        factors = firebase_app_inputs.get()
+        print(factors)
+        print(type(factors))
+
 
         while True:
             # Server Status
-            status = firebase.get("/Python AI Server/Server Status", "")
-            status_dict = list(status.values())[0]
-            if status_dict['Status'] == "Closed":
-                break
+            # status = firebase_app.get("/Python AI Server/Server Status", "")
+            # status_dict = list(status.values())[0]
+            # if status_dict['Status'] == "Closed":
+            #     break
 
-            factors = firebase.get("/Python AI Server/Input Factors", "")
+            factors = firebase_app_inputs.get()
             done = False
 
             for entry in factors:
-                """print("Entered")
-                print(factors)"""
+                #print("Outer Key: %s", entry)
+                if entry == 'Done' and factors[entry] == "Yes":
+                    inputs_count += 1
 
-                for inner_entry in factors[entry]:
-                    #print(inner_entry)
-                    if inner_entry == 'Done' and factors[entry][inner_entry] == "Yes":
-                        """print("Entry: " + entry)
-                        print("Inner Entry: " + inner_entry)
-                        print("Factor: " + factors[entry][inner_entry])"""
-                        factor_name = entry
-                        name = factor_name
+                    parking = int(factors['Factors']['Parking'])
+                    distance = float(factors['Factors']['Percentage Distance'])
+                    refundable_deposit = int(factors['Factors']['Refundable Deposit'])
 
-                        current_factor_list = firebase.get("/Python AI Server/Input Factors/" + factor_name, "")
-
-                        parking = int(current_factor_list['Factors']['Parking'])
-                        distance = float(current_factor_list['Factors']['Percentage Distance'])
-                        refundable_deposit = int(current_factor_list['Factors']['Refundable Deposit'])
-
-                        done = True
-                        break
-
-            """counter += 1
-
-            if counter == 5:
-                factor_data = {
-                    "Parking": "1",
-                    "Percentage Distance": "70.0",
-                    "Refundable Deposit": "1"
-                }
-
-                data = {
-                    "Factors": factor_data,
-                    "Done": "Yes"
-                }
-
-                for field in data:
-                    test = firebase.put("/Python AI Server/Input Factors/" + first_factor_name, field, data[field])
-                #print(test)"""
+                    done = True
+                    break
 
             if done:
                 break
 
+        if True:
+            firebase_predictions_check = db.reference("/Python AI Server/Predictions")
+            predictions_check = firebase_predictions_check.get()
+            if predictions_check is None:
+                firebase_app_predictions = db.reference("/Python AI Server/Predictions/0")
+                create_prediction_results()
 
-        print("Found factor inputs at: " + name)
+                predictions_count = 0
+                print("Predictions count is at %s" % predictions_count)
+                firebase_app_predictions = db.reference(predictions_path + str(predictions_count))
+
+        print("Predictions count is at %s" % predictions_count)
+
+        print()
+        print(parking)
+        print(distance)
+        print(refundable_deposit)
+
+        print("Found factor inputs at: 0")
 
         main_prediction = main.aggregation(parking, distance, refundable_deposit)
         db_prediction = main_prediction[1]
@@ -177,85 +175,28 @@ def mainloop():
             "Probability" : str(main_prediction[0])
         }
 
-        db_output = firebase.post("/Python AI Server/Predictions", db_data)
-        print("[POST] - " + str(db_output))
-        entry_names.append(db_output['name'])
-
+        # global firebase_app_predictions
+        # firebase_app_predictions_child = db.reference(predictions_path + str(predictions_count))
+        print("Predictions path is at %s" %predictions_path)
+        print("Predictions count is at %s" %predictions_count)
+        firebase_app_predictions.update(db_data)
+        predictions_count += 1
         print()
+        firebase_app_predictions = db.reference("/Python AI Server/Predictions")
+        predictions_dict = firebase_app_predictions.get()
+        print(predictions_dict)
 
-        firebase.delete("/Python AI Server/Input Factors", name)
+        # predictions_count = 0
+        # for entry in predictions_dict:
+        #     print(entry)
+        #     predictions_count += 1
+        # print("Number of Predictions Available: %s" %predictions_count)
 
-        # Server Status
-        status = firebase.get("/Python AI Server/Server Status", "")
-        status_dict = list(status.values())[0]
-        if status_dict['Status'] == "Closed":
-            break
+        firebase_app_inputs.delete()
 
+        inputs_count -= 1
         create_input_factors()
-
-        count += 1
-        time.sleep(0.2)
-
-
-    """while count <= 10:
-        parking = random.randint(0, 1)
-        distance = random.uniform(0.0, 100.0)
-        refundable_deposit = random.randint(0, 1)
-
-        main_prediction = main.aggregation(parking, distance, refundable_deposit)
-        db_prediction = main_prediction[1]
-        db_prediction_result = re.sub('\W+', ' ', db_prediction)
-        db_prediction_result = db_prediction_result[1: (len(db_prediction_result) - 1)]
-        if db_prediction_result == "Won t show up":
-            db_prediction_result = "Won't show up"
-        print(db_prediction_result)
-
-        db_data_factors = {
-            "Parking": str(parking),
-            "Percentage Distance": str(distance),
-            "Refundable Deposit": str(refundable_deposit)
-        }
-
-        db_data = {
-            "Factors" : db_data_factors,
-            "Prediction" : db_prediction_result,
-            "Probability" : str(main_prediction[0])
-        }
-
-        if count == 1:
-            db_output = ""
-
-            for field in db_data:
-                db_output = firebase.put("/Python AI Server/Predictions/" + first_name, field, db_data[field])
-
-            print("[PUT] - " + str(db_output))
-
-            entry_names.append(first_name)
-        else:
-            db_output = firebase.post("/Python AI Server/Predictions", db_data)
-            print("[POST] - " + str(db_output))
-            entry_names.append(db_output['name'])
-
-        print()
-
-        count += 1
-        time.sleep(0.2)"""
+        # time.sleep(0.05)
 
 
 mainloop()
-
-
-"""dictn = firebase.get("/Python AI Server/Predictions", "")
-print(dictn)
-print()
-
-for entry in dictn:
-    entry_names.append(entry)
-
-for name in entry_names:
-    print(name)
-
-print()"""
-
-#result = firebase.get("/Python AI Server/Predictions/" + entry_names[4], "")
-#print(result)
--- No Shows Prediction AI/Server_PreTester.py
+++ No Shows Prediction AI/Server_PreTester.py
@@ -1,34 +1,119 @@
+import time
+
 import main
-from firebase import firebase
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
 
 
 """result = main.aggregation(1, 70.0, 1)
 print(result[0])
 print(result[1])"""
 
-firebase = firebase.FirebaseApplication("https://resresapp-default-rtdb.asia-southeast1.firebasedatabase.app/", None)
+# Set our Google Firebase Database
+cred = credentials.Certificate("resresapp-firebase-adminsdk-f9ios-dfb77556fd.json")
+firebase_admin.initialize_app(cred, {
+    "databaseURL" : "https://resresapp-default-rtdb.asia-southeast1.firebasedatabase.app/"
+})
 
-factors = firebase.get("/Python AI Server/Input Factors", "")
+firebase_app_status = db.reference("/Python AI Server/Server Status")
+inputs_path = "/Python AI Server/Inputs/"
 
-factor_data = {
-    "Parking": "1",
-    "Percentage Distance": "0.0",
-    "Refundable Deposit": "1"
-}
+# firebase_app_inputs = db.reference(inputs_path + str(0))
+#
+# number = 25.0 * 1
+# distance = str(number)
+#
+# factors = firebase_app_inputs.get()
+#
+# factor_data = {
+#     "Parking": "0",
+#     "Percentage Distance": distance,
+#     "Refundable Deposit": "0"
+# }
+#
+# data = {
+#     "Factors": factor_data,
+#     "Done": "Yes"
+# }
+#
+# firebase_app_inputs.update(data)
 
-data = {
-    "Factors": factor_data,
-    "Done": "Yes"
-}
+# count = 0
+# firebase_app_inputs = db.reference(inputs_path + str(count))
+#
+# number = 25.0 * count
+# distance = str(number)
+#
+# factors = firebase_app_inputs.get()
+#
+# factor_data = {
+#     "Parking": "0",
+#     "Percentage Distance": distance,
+#     "Refundable Deposit": "0"
+# }
+#
+# data = {
+#     "Factors": factor_data,
+#     "Done": "Yes"
+# }
+#
+# firebase_app_inputs.update(data)
 
-for field in data:
-    test = firebase.put("/Python AI Server/Input Factors/" + list(factors)[0], field, data[field])
+db_id = 0
 
-"""print(factors)
-print()"""
+for count in range(5):
+    print("Count is at %s" %count)
+    print("ID is at %s" %db_id)
+    firebase_app_inputs = db.reference(inputs_path + str(db_id))
+    factors = firebase_app_inputs.get()
+    print(factors)
 
-status = firebase.get("/Python AI Server/Server Status", "")
-entry = list(status.values())[0]
-key = list(entry)[0]
+    while factors["Done"] == "Yes" and db_id >= 0:
+        firebase_app_inputs = db.reference(inputs_path + str(db_id))
+        factors = firebase_app_inputs.get()
 
-status_output = firebase.put("/Python AI Server/Server Status/" + list(status)[0], key, 'Closed')
+        while factors is None:
+            firebase_app_inputs = db.reference(inputs_path + str(db_id))
+            factors = firebase_app_inputs.get()
+
+        db_id -= 1
+
+    if db_id == -1:
+        db_id = 0
+
+    firebase_app_inputs = db.reference(inputs_path + str(db_id))
+
+    number = 25.0 * count
+    distance = str(number)
+
+    # factors = firebase_app_inputs.get()
+
+    factor_data = {
+        "Parking": "0",
+        "Percentage Distance": distance,
+        "Refundable Deposit": "0"
+    }
+
+    data = {
+        "Factors": factor_data,
+        "Done": "Yes"
+    }
+
+    firebase_app_inputs.update(data)
+    time.sleep(0.5)
+
+# predictions_path = "/Python AI Server/Predictions/"
+#
+# for count in range(5):
+#     firebase_app_predictions = db.reference(predictions_path + str(count))
+#     factors = firebase_app_predictions.get()
+#     print("Deleting: %s" %factors)
+#     firebase_app_predictions.delete()
+#     time.sleep(0.25)
+
+# status = firebase.get("/Python AI Server/Server Status", "")
+# entry = list(status.values())[0]
+# key = list(entry)[0]
+
+#status_output = firebase.put("/Python AI Server/Server Status/" + list(status)[0], key, 'Closed')
