643f70fa3edfb985806ff005d8e74e13fc06c5d7
Imalsha <dinithimale@gmail.com>
2022-03-21 22:45:05 +0530

    started working on the splash screen

---
+++ lib/splashScreen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class splashScreen extends StatelessWidget {
+  const splashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+
+      ),
+    );
+  }
+}
